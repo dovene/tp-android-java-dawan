36fb8a9aeb52e405eb8138340c8d9ad2e971404e
dovene <adovene@gmail.com>
2024-11-18 21:24:57 +0100

    add menu and toolbar

--- app/src/main/java/com/dov/calculator/RichFormActivity.java
+++ app/src/main/java/com/dov/calculator/RichFormActivity.java
@@ -1,14 +1,9 @@
 package com.dov.calculator;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.view.Menu;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RichFormActivity extends AppCompatActivity {
 
@@ -16,5 +11,13 @@ public class RichFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rich_form);
+        setSupportActionBar(findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_property, menu);
+        return true;
     }
 }
