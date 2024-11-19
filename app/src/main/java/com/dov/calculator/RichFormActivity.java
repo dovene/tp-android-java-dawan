41187be30834c4140b6476ab1aa1bba9597f552a
dovene <adovene@gmail.com>
2024-11-19 09:26:48 +0100

    handle menu items click

--- app/src/main/java/com/dov/calculator/RichFormActivity.java
+++ app/src/main/java/com/dov/calculator/RichFormActivity.java
@@ -1,9 +1,15 @@
 package com.dov.calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.dov.calculator.preferences.SettingsActivity;
 
 public class RichFormActivity extends AppCompatActivity {
 
@@ -14,10 +20,21 @@ public class RichFormActivity extends AppCompatActivity {
         setSupportActionBar(findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_property, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else if (item.getItemId() == R.id.action_save) {
+            Toast.makeText(this, "Bien enregistré", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
